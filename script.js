404fb73a596972f8742b533293581e4f3e9a457f
Matt <matthewjpetroff@gmail.com>
2020-07-13 14:37:18 -0400

    Fix AM/PM Bug

--- script.js
+++ script.js
@@ -23,14 +23,14 @@ function updateClock() {
 	var today = new Date();
 
 	var hour = today.getHours();
-	if (hour > 12) {
-		hour = hour - 12;
-	}
-
 	if (hour < 12) {
 		var ampm = 'AM';	
 	} else {
 		var ampm = 'PM';
+	}	
+
+	if (hour > 12) {
+		hour = hour - 12;
 	}
 
 	hour = padZeros(hour);
